ac2c298448ca4166d1d2a42149624093e1570e9d
sowmya-peddina <sowmyapeddina@gmail.com>
2026-06-25 14:30:26 +0530

    feat(cluster): implement simple ClusterService (add/remove/get/size)

--- hci_repo_structure/src/cluster/cluster_service.py
+++ hci_repo_structure/src/cluster/cluster_service.py
@@ -1,4 +1,51 @@
-# cluster service
+# cluster service - Edited to provide simple cluster behavior
 
 class ClusterService:
-    pass
+    """A minimal cluster service used by tests.
+
+    It maintains a set of node identifiers and provides simple operations:
+    - add_node(node): adds a node (no-op if already present), returns True if added
+    - remove_node(node): removes a node, returns True if removed
+    - get_nodes(): returns a list of nodes
+    - size(): returns number of nodes
+
+    The implementation uses an internal set to keep nodes unique while preserving
+    insertion order for get_nodes using a list for deterministic order.
+    """
+
+    def __init__(self, nodes=None):
+        self._nodes_set = set()
+        self._nodes_order = []
+        if nodes:
+            for n in nodes:
+                self.add_node(n)
+
+    def add_node(self, node):
+        """Add a node to the cluster.
+
+        Returns True if the node was added, False if it was already present.
+        """
+        if node in self._nodes_set:
+            return False
+        self._nodes_set.add(node)
+        self._nodes_order.append(node)
+        return True
+
+    def remove_node(self, node):
+        """Remove a node from the cluster.
+
+        Returns True if the node was removed, False if it was not present.
+        """
+        if node not in self._nodes_set:
+            return False
+        self._nodes_set.remove(node)
+        self._nodes_order.remove(node)
+        return True
+
+    def get_nodes(self):
+        """Return the list of nodes in insertion order."""
+        return list(self._nodes_order)
+
+    def size(self):
+        """Return the number of nodes in the cluster."""
+        return len(self._nodes_order)
